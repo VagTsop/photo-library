d1cb7e609abc2a9e3e092134dbd703423181a09b
VagTsop <vatsop52@gmail.com>
2024-09-27 14:33:28 +0300

    rremove unused imports

--- product-app/src/app/my-components/header/header.component.ts
+++ product-app/src/app/my-components/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { PhotoService } from 'src/app/service/photo-service';
 
 @Component({
   selector: 'app-header',
